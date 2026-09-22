ca8430a006021fc3f1ec4066004f4e04d856218b
Novi Chochalman <novichochalman@gmail.com>
2026-09-22 08:27:41 +0700

    feat: configure static HTML export and GitHub Pages deployment workflow

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import { company } from "@/data/company";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = company.domain;
 
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from "next";
 import { company } from "@/data/company";
 import { productCategories } from "@/data/products";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = company.domain;
   const lastModified = new Date();
